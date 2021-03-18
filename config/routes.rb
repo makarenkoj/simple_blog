4b0af8821c886d2d5750afff4f361f014895e38c
makarenkoj <makarenkoj53@gmail.com>
2021-03-18 18:02:07 +0200

    create buton

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    get 'users/sign_out', to: 'devise/sessions#destroy', as: 'logout'
-  end
-  devise_for :users
-  root 'posts#index'
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    devise_scope :user do
+      get 'users/sign_out', to: 'devise/sessions#destroy', as: 'logout'
+    end
+    devise_for :users
+    root 'posts#index'
 
-  resources :posts
-  resources :users
+    resources :posts
+    resources :users
+  end
 end
