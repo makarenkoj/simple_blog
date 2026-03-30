1a0553d7b131dfca8714516f71534bb501951144
makarenkoj <makarenkoj53@gmail.com>
2026-03-30 20:09:10 +0300

    fix code style

--- spec/system/category_cards_spec.rb
+++ spec/system/category_cards_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Category Cards interaction", type: :system do
+RSpec.describe 'Category Cards interaction', type: :system do
   include_context 'base'
 
-  let!(:category) { create(:category, name: "Ruby") }
+  let!(:category) { create(:category, name: 'Ruby') }
   let(:locale) { :uk }
 
   before do
@@ -11,38 +11,38 @@ RSpec.describe "Category Cards interaction", type: :system do
     # driven_by(:selenium_chrome)
   end
 
-  describe "Unauthenticated user" do
+  describe 'Unauthenticated user' do
     before { visit categories_path(locale: locale) }
 
-    it "navigates to category show page when clicking the card itself" do
+    it 'navigates to category show page when clicking the card itself' do
       find("[data-controller='category-card']").click
 
-      expect(page).not_to have_content("Категорію не знайдено!")
+      expect(page).not_to have_content('Категорію не знайдено!')
       expect(page).to have_current_path(category_path(category, locale: locale))
-      expect(page).to have_selector('h1', text: "Ruby", wait: 2)
+      expect(page).to have_selector('h1', text: 'Ruby', wait: 2)
     end
 
     it "does not navigate to category when clicking 'Sign in to subscribe'" do
       within("[data-controller='category-card']") do
-        click_link "Увійти, щоб підписатися"
+        click_link 'Увійти, щоб підписатися'
       end
 
       expect(page).to have_current_path(new_user_session_path(locale: locale))
     end
   end
 
-  describe "Authenticated user" do
+  describe 'Authenticated user' do
     before do
       sign_in current_user
       visit categories_path(locale: locale)
     end
 
-    it "subscribes but stays on the same page when clicking the subscribe button" do
+    it 'subscribes but stays on the same page when clicking the subscribe button' do
       within("[data-controller='category-card']") do
-        find(".z-30 form button").click 
+        find('.z-30 form button').click
       end
 
-      expect(page).to have_content("Підписані на категорію ruby")
+      expect(page).to have_content('Підписані на категорію ruby')
       expect(page).to have_current_path(categories_path(locale: locale))
       expect(current_user.category_preferences.count).to eq(1)
     end
