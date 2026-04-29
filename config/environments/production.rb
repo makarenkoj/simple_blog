06b6e188f8793fc3a46ad2649e17030277ae3a3a
makarenkoj <makarenkoj53@gmail.com>
2026-04-29 17:21:19 +0300

    reformat images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
   config.active_storage.service = :cloudflare
+  config.active_storage.variant_processor = :vips
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
