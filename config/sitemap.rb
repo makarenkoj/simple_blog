a3176d476e85957cf04fcd19dba9741964dd62e5
makarenkoj <makarenkoj53@gmail.com>
2026-04-30 21:04:33 +0300

    cnaooac  kcscjs jsjs FIX

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -22,7 +22,7 @@ class R2Adapter
                         key: location.path_in_public,
                         body: file,
                         content_type: mime_type
-                      )
+                        )
     end
   end
 end
@@ -30,6 +30,7 @@ end
 bucket_name = ENV.fetch('R2_BUCKET_NAME')
 sitemaps_host = ENV.fetch('R2_PUB_DEV_URL', 'https://pub-b5117945ddf24c73be1b37aa13e64f80.r2.dev')
 default_host = ENV.fetch('DEFAULT_HOST', 'helpbooost.com')
+host_url = "https://#{default_host}"
 
 options = { host: default_host, protocol: 'https' }
 Rails.application.routes.default_url_options = options
@@ -40,7 +41,7 @@ if defined?(ActiveStorage::Current)
   ActiveStorage::Current.url_options = options
 end
 
-SitemapGenerator::Sitemap.default_host = "https://#{default_host}"
+SitemapGenerator::Sitemap.default_host = host_url
 SitemapGenerator::Sitemap.sitemaps_host = sitemaps_host
 SitemapGenerator::Sitemap.public_path = 'tmp/'
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
@@ -50,18 +51,18 @@ SitemapGenerator::Sitemap.adapter = R2Adapter.new(bucket_name,
                                                   aws_secret_access_key: ENV.fetch('R2_SECRET_ACCESS_KEY'),
                                                   aws_region: 'auto',
                                                   endpoint: ENV.fetch('R2_ENDPOINT')
-                                                )
+                                                  )
 
 SitemapGenerator::Sitemap.create(compress: true, include_root: false) do
   [:uk, :en, :it].each do |locale|
-    add root_path(locale: locale), changefreq: 'daily', priority: 1.0
+    add root_url(locale: locale, host: host_url), changefreq: 'daily', priority: 1.0
 
     Post.find_each do |post|
-      add post_path(post, locale: locale), lastmod: post.updated_at, changefreq: 'weekly'
+      add post_url(post, locale: locale, host: host_url), lastmod: post.updated_at, changefreq: 'weekly'
     end
 
     Category.find_each do |category|
-      add category_path(category, locale: locale), changefreq: 'weekly'
+      add category_url(category, locale: locale, host: host_url), changefreq: 'weekly'
     end
   end
 end
