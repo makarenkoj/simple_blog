721f385d2f818c676d075ab48af95ccb74e46dcc
makarenkoj <makarenkoj53@gmail.com>
2026-05-24 00:10:18 +0300

    fix n+1 posts

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,7 +8,7 @@ class CategoriesController < ApplicationController
     query = query.joins(:categories).where(categories: { id: params[:category_ids] }).distinct if params[:category_ids].present?
     @pagy, @posts = pagy(query.order(created_at: :desc), limit: 5)
     post_ids = @category.posts.select(:id)
-    @category_counts = Category.joins(:posts).where(posts: { id: post_ids }).where.not(id: @category.id).group('categories.id').count
+    @category_counts = Category.joins(:posts).where(posts: { id: post_ids }).where.not(id: @category.id).group('categories.id').size
     @filter_categories = Category.where(id: @category_counts.keys).order(:name)
 
     render 'posts/index'
